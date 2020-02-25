cc06b83d266956d375fd1312f51017b67f93febf
Kai Marshall <k.marshall@setonhill.edu>
2020-02-25 16:27:14 -0500

    Initial Commit

---
+++ Homework 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 4
+//
+//  Created by Kai Marshall on 2/25/20.
+//  Copyright © 2020 Kai Marshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
