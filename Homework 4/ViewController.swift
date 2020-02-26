3bf901803be3499cd1fc232c5b32fd3d6b238a61
Kai Marshall <k.marshall@setonhill.edu>
2020-02-26 09:39:07 -0500

    Ready

--- Homework 4/ViewController.swift
+++ Homework 4/ViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    @IBAction func RedAlert(_ sender: Any) {
+        let RedAlert = UIAlertController(title:  " Important Message", message : "Your favorite color is Red, you aare Dragon.", preferredStyle: .alert)
+        RedAlert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Defualt action"), style: . `default`, handler: { _ in } ))
+        self.present(RedAlert,animated: true,completion: nil)
+        
+    }
+    @IBAction func BlueAlert(_ sender: Any) { 
+        let BlueAlert = UIAlertController(title: " Important Message", message: "Your favorite color is Blue, you are a Shark.", preferredStyle: .alert)
+        BlueAlert.addAction(UIAlertAction(title:
+        NSLocalizedString("OK", comment: "Defualt action"),
+        style: . `default`, handler: { _ in } ))
+        self.present(BlueAlert,animated: true,completion: nil)
+                                          
+    }
+    
+    
+    
+    @IBAction func loginAlert(_ sender: Any) {
+        let loginAlert = UIAlertController(title:  " Important Update", message : "Login Suceeded.", preferredStyle: .alert)
+        loginAlert.addAction(UIAlertAction(title:
+            NSLocalizedString("OK", comment: "Defualt action"),
+                                           style: .`default`, handler: { _ in} ))
+        self.present(loginAlert,animated: true,completion: nil)
+                                         
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
